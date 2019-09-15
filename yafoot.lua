a8751a789c7037206b5aa3921fd816b3aded29d2
Keiichiro Shikano <k16.shikano@gmail.com>
2019-09-15 22:12:36 +0900

    it works!

--- yafoot.lua
+++ yafoot.lua
@@ -1,3 +1,21 @@
+pagenumber = 0
+
+local file = io.open(tex.jobname..".fht", "r")
+io.input(file)
+local pages_ftn_ht = {}
+local has_pages_ftn_ht = false
+for line in file:lines() do
+   local line_arr = {}
+   for i in string.gmatch(line, "%S+") do
+      table.insert(line_arr, tonumber(i))
+   end
+   table.insert(pages_ftn_ht, line_arr)
+   has_pages_ftn_ht = true
+end
+io.close(file)
+local file = io.open(tex.jobname..".fht", "w")
+io.close(file)
+
 push_footnotes_below_lines = function (head, group)
    for item in node.traverse_id(node.id("whatsit"), head) do
       local is_footnote = node.has_attribute(item, 100)
@@ -6,7 +24,6 @@ push_footnotes_below_lines = function (head, group)
          local footnote = node.copy(tex.box[is_footnote])
          head, new = node.insert_after(head, item, footnote)
          node.set_attribute(new, 200, is_footnote)
-
          item = item.next
          new.width = 0
       end
@@ -14,69 +31,61 @@ push_footnotes_below_lines = function (head, group)
    return head
 end
 
-crush_height_of_hlist = function (head, bgroup) 
-   for item in node.traverse_id(node.id("vlist"), head) do
-   end
-   return head
-end
-
 -- crush vlist under hlist
 crush_height_of_vlist = function (head, group, size)
    for list in node.traverse(head) do
-      if list.id == node.id("vlist") or list.id == node.id("hlist") or list.id == node.id("rule")
+      if list.id == node.id("hlist")
       then
-         local h = list.height
-         local d = list.depth
-         
-         for item in node.traverse(list) do
-            local f = node.has_attribute(item, 200)
-            
-            if f
+      for item in node.traverse(list) do
+         local f = node.has_attribute(item, 200)
+         if f
+         then
+            if not node.has_attribute(item, 300)
             then
-               if not node.has_attribute(item, 300)
-               then
-                  -- need to shift the page bottom upword to make a room for footins.
-                  -- this code is still wrong, though... 
-                  node.set_attribute(item, 300, item.height)
-                  item.height = h-item.height
-                  item.depth = d-item.depth
-               else
-                  item.height = 0
-                  item.depth = 0
-               end
+               node.set_attribute(item, 300, item.height+item.depth)
+               item.height = 0
+               item.depth = 0
+            else
             end
          end
       end
+      end
    end
+   
    return head
 end
 
 move_footnote_bottom = function (page_head, group, s)
    local yaftnins = node.new("vlist")
-   yaftnins.list, new = node.insert_before(yaftnins.list, yaftnins.list, node.copy(tex.box.yaftnins))
+   void = node.new("vlist")
+   yaftnins.list, new = node.insert_before(yaftnins.list, yaftnins.list, node.copy(tex.box.footins))
+   local yaftnins_ht = 0
    
-   recur = function (head, under_hlist, page_head)
+   recur = function (head, isftn, page_head)
       for item in node.traverse(head) do
          if item.id == node.id("vlist")
          then
-            page_head = recur_vlist(item, under_hlist, page_head)
+            page_head = recur_vlist(item, isftn, page_head)
          elseif item.id == node.id("hlist")
          then
-            page_head = recur_hlist(item, page_head)
+            page_head = recur_hlist(item, isftn, page_head)
          end
       end
       return page_head
    end
 
-   recur_hlist = function (hlist, page_head)
-      for vlist in node.traverse_id("vlist", hlist) do
-         page_head = recur(hlist.head, vlist, page_head)
+   recur_hlist = function (hlist, isftn, page_head)
+      for item in node.traverse(hlist.head) do
+         if item.id == node.id("hlist") or item.id == node.id("vlist")
+         then
+            page_head = recur(item, isftn, page_head)
+         end
       end
       return page_head
    end
 
-   recur_vlist = function (head, under_hlist, page_head)
-      for vlist in node.traverse(head) do
+   recur_vlist = function (head, isftn, page_head)
+      for vlist in node.traverse_id(node.id("vlist"), head) do
          local footnotebox = node.has_attribute(vlist, 200)
          
          if footnotebox then
@@ -86,34 +95,68 @@ move_footnote_bottom = function (page_head, group, s)
             then
                yaftnins.list, new = node.insert_after(yaftnins.list, yaftnins.tail, footnote)
             end
+            page_head = recur(vlist.head, true, page_head)
          else
             if vlist.head
             then
-               page_head = recur(vlist.head, under_hlist, page_head)
+               page_head = recur(vlist.head, isftn, page_head)
             end
          end
       end
       return page_head
    end
    
-   page_head = recur(page_head, nil, page_head)
+   recur(page_head, false, page_head)
+
+   local split_top
    if yaftnins.list
    then
       tex.box.footins = node.copy(node.vpack(yaftnins.list))
    end
-
+   
    return page_head
 end
 
-function buildpage_info(groupcode)
-   lastnode = tex.lists.contrib_head
-   if lastnode
+-- called after the page was built (or tried to build)
+function page_ftn_height(groupcode)
+   if groupcode == "after_output"
+   then
+      pagenumber = pagenumber+1
+      if has_pages_ftn_ht
       then
-      temp = node.tail(lastnode)
-      if temp and temp.id == node.id("hlist")
+         for i,l in ipairs(pages_ftn_ht) do
+            if tonumber(l[1]) == pagenumber
+            then
+               tex.setdimen("global", "my@tcb@ftn@height", tonumber(l[2]))
+            end
+         end
+      end
+   elseif groupcode == "vmode_par" or groupcode == "hmode_par"
+   then
+      ftn_ht = get_ftnheight(tex.lists.contrib_head)
+      if ftn_ht > 0
       then
-         print("\nMoving page contents at height = "..temp.height.."sp")
+         tex.setdimen("global", "my@tcb@ftn@height", ftn_ht + tex.getdimen("footskip"))
+         file = io.open(tex.jobname..".fht", "a")
+         io.output(file)
+         io.write(pagenumber.." "..ftn_ht + tex.getdimen("footskip").."\n")
+         io.close(file)
       end
    end
+end
+
+get_ftnheight = function (n)
+   local ftnheight = 0
+   for list in node.traverse(n) do
+      if node.has_attribute(list, 300)
+      then
+         ftnheight = ftnheight + node.get_attribute(list, 300)
+         node.unset_attribute(list, 300)
+      elseif list.head
+      then
+         ftnheight = ftnheight + get_ftnheight (list.head)
+      end
+   end
+   return ftnheight
 end
 
